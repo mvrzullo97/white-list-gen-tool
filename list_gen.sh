8383cfe637f1a623638024f3e6ea7cd8ad6f88d6
Alessio Marzullo <alessio.marzullo@txtgroup.com>
2024-07-05 17:08:08 +0200

    aggiunte funzionalità finali

--- list_gen.sh
+++ list_gen.sh
@@ -5,14 +5,15 @@
 # usage menu
 echo
 echo "---------------------- Usage ----------------------"
-echo -e "\n   bash $0\n\n    -n <number of white-list to generate> \n    -t <list_type> (WI or WL) \n    -s <service_provider code> (ex. 151) \n    -c <toll_charger> (ex. 6) \n    -a <apduIdentifier> \n    -p <progressive_number> (to generate plate automatically) \n    -f <first_plate_chars> (ex. AB) \n    -l <last_plate_chars> (ex. CD) \n    -r <PRG_white_list> \n"
+echo -e "\n   bash $0\n\n    -n <number of white-list to generate> \n    -t <list_type> (WI or WL) \n    -o <opt. delete_file.XML> (y-n)\n    -s <service_provider code> (ex. 151) \n    -c <toll_charger> (ex. 6) \n    -a <apduIdentifier> \n    -p <progressive_number> (to generate plate automatically) \n    -f <first_plate_chars> (ex. AB) \n    -l <last_plate_chars> (ex. CD) \n    -r <PRG_white_list> \n"
 echo
 
-while getopts n:t:s:c:a:p:f:l:r: flag
+while getopts n:t:o:s:c:a:p:f:l:r: flag
 do
     	case "${flag}" in
 		n) n=${OPTARG};;
         t) LIST_TYPE=${OPTARG};;
+        o) FLAG_gen=${OPTARG};;
         s) S_PROVIDER=${OPTARG};;
         c) T_CHARGER=${OPTARG};;
         a) C_APDU=${OPTARG};;
@@ -23,6 +24,13 @@ do
 		\?) echo -e "\n Argument error! \n"; exit 0 ;;
 	esac
 done
+
+# params check
+if [ $# != 18 ] && [ $# != 20 ] ; then
+        echo "Argument error: please digit right command."
+	echo
+	exit 0
+fi
 
 # functions
 function generate_PAN 
@@ -84,22 +92,42 @@ function extract_offset_pad
     echo ${final_code}
 }
 
+function extract_WL_type 
+{
+    LIST_TYPE=$1
+    if [ $LIST_TYPE == "WI" ] ; then
+        LIST_TYPE="WIWI"
+    else
+        LIST_TYPE="WFWF"
+    fi
+    echo ${LIST_TYPE}
+}
+
+function extract_ACK_type 
+{
+    LIST_TYPE=$1
+    if [ $LIST_TYPE == "WIWI" ] ; then
+        ACK_TYPE="WIAK"
+    else
+        ACK_TYPE="WFAK"
+    fi
+    echo ${ACK_TYPE}
+}
+
+
 # vars delcaration
 current_timestamp=$(date +"%Y%m%d")
 OUT_DIR="OUT_DIR_WL"
 tmp_filename_WL="tmp_filename_white_list.xml"
 
-if [ $LIST_TYPE == "WI" ] ; then
-    LIST_TYPE="WIWI"
-else
-    LIST_TYPE="WFWF"
-fi
+
+LIST_TYPE=$(extract_WL_type $LIST_TYPE)
 
 # create OUT_DIR if not exist
 if ! [ -d $OUT_DIR ] ; then
 	mkdir $OUT_DIR
 	path_OUT_dir=$(realpath $OUT_DIR)
-    echo -e "create '$OUT_DIR' at path: '$path_OUT_dir' \n"
+    echo -e "...create '$OUT_DIR' at path: '$path_OUT_dir' \n"
     chmod 0777 "$path_OUT_dir"
 else
 	path_OUT_dir=$(realpath $OUT_DIR)
@@ -110,26 +138,32 @@ MIN=$(expr $PRG + 1)
 MAX=$(expr $MIN + $n - 1)
 list_PRG=( $(seq $MIN $MAX) )
 
-PRG=${list_PRG[0]} # to do, cambiare dopo inserendo indicizzazione
+# start the loop
+
+for ((i=0; i<n; i++)) 
+do
+PRG=${list_PRG[i]}
+
+echo -e "...create white list n° $(expr $i + 1) \n"
 
 # gen PAN
 PAN=$(generate_PAN $PRG)
-echo -e "...generate PAN: $PAN \n "
+echo -e "...generate PAN: $PAN"
 
 # gen PLATE
 PLATE=$(generate_PLATE $PRG $plate_f $plate_l)
 echo -e "...generate PLATE: $PLATE \n"
 
 HEX_PLATE=$(convert_PLATE_to_HEX $PLATE)
-echo -e "...generate HEX_PLATE: $HEX_PLATE \n"
+#echo -e "...generate HEX_PLATE: $HEX_PLATE \n"
 
 # create tmp file for the WL
 touch "$path_OUT_dir/$tmp_filename_WL"
 chmod 0777 "$path_OUT_dir/$tmp_filename_WL"
 
-echo -e "...create '$tmp_filename_WL' at path: $(realpath $tmp_filename_WL): OK \n"
+#echo -e "...create '$tmp_filename_WL' at path: $(realpath $tmp_filename_WL): OK \n"
 
-echo -e "...generate white list with: Service Provider $S_PROVIDER, Toll Charger $T_CHARGER and apduIdentifer $C_APDU: OK \n"
+#echo -e "...generate white list with: Service Provider $S_PROVIDER, Toll Charger $T_CHARGER and apduIdentifer $C_APDU: OK \n"
 
 # insert params into WL file
 cat << EOF > "$path_OUT_dir/$tmp_filename_WL"
@@ -193,7 +227,7 @@ EOF
 
 # modify white list filename
 
-# pattern filename: DA06A56.F<naz_SP>00<cod_SP(5 chars)>T<naz_TC>00<cod_TC (5 chars)>.SET.<list_TYPE>.<unix_timestamp>.000<PRG (10 chars)>.XML
+# pattern filename WL: DA06A56.F<naz_SP>00<cod_SP(5 chars)>T<naz_TC>00<cod_TC (5 chars)>.SET.<list_TYPE>.<unix_timestamp>.000<PRG (10 chars)>.XML
 
 const_DA_A="DA06A56.F"
 naz_SP="IT" #per ora costante
@@ -204,18 +238,76 @@ unix_timestamp=$(date +%s)
 
 fn_S_PROVIDER=$(extract_offset_pad $S_PROVIDER 5)
 fn_T_CHARGER=$(extract_offset_pad $T_CHARGER 5)
+
 fn_PRG_WL=$(extract_offset_pad $fn_PRG_WL 10)
 
- 
 filename_WL="$const_DA_A$naz_SP$fn_S_PROVIDER$const_T$naz_TC$fn_T_CHARGER.$const_SET.$LIST_TYPE.$unix_timestamp.$fn_PRG_WL.XML"
 
 mv "$path_OUT_dir/$tmp_filename_WL" "$path_OUT_dir/$filename_WL"
-echo -e "...change filename from '$tmp_filename_WL' to '$filename_WL': OK \n"
+#echo -e "...change filename from '$tmp_filename_WL' to '$filename_WL': OK \n"
 
 filename_WL_ZIP="$filename_WL.ZIP"
 zip -r -q "$path_OUT_dir/$filename_WL_ZIP" "$path_OUT_dir/$filename_WL"
-echo "...zipped file: $filename_WL_ZIP"
+#echo "...zipped file white list: '$filename_WL_ZIP'"
+#echo
+
+# to do creare il file di ack relativo (NOTA: SP e TC sono swappati in caso di file di ack!)
+# pattern filename ACK: DA06A56.F<naz_TC>00<cod_TC (5 chars)>T<naz_SP>00<cod_SP(5 chars)>.SET.<ACK_list_TYPE>.000<PRG (10 chars)>.XML
+
+ACK_TIPE=$(extract_ACK_type $LIST_TYPE)
+filename_ACK="$const_DA_A$naz_TC$fn_T_CHARGER$const_T$naz_SP$fn_S_PROVIDER.$const_SET.$ACK_TIPE.$fn_PRG_WL.XML"
+
+cat << EOF > "$path_OUT_dir/$filename_ACK"
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<infoExchange>
+    <infoExchangeContent>
+        <apci>
+            <aidIdentifier>3</aidIdentifier>
+            <apduOriginator>
+                <countryCode>0110000001</countryCode>
+                <providerIdentifier>${T_CHARGER}</providerIdentifier>
+            </apduOriginator>
+            <informationSenderId>
+                <countryCode>0110000001</countryCode>
+                <providerIdentifier>${T_CHARGER}</providerIdentifier>
+            </informationSenderId>
+            <informationRecipientId>
+                <countryCode>0110000001</countryCode>
+                <providerIdentifier>${S_PROVIDER}</providerIdentifier>
+            </informationRecipientId>
+            <apduIdentifier>927</apduIdentifier>
+            <apduDate>20240222143716Z</apduDate>
+        </apci>
+        <adus>
+            <ackAdus>
+                <AckAdu>
+                    <apduIdentifier>${C_APDU}</apduIdentifier>
+                    <apduAckCode>2</apduAckCode>
+                    <actionCode>0</actionCode>
+                </AckAdu>
+            </ackAdus>
+        </adus>
+    </infoExchangeContent>
+</infoExchange>
+EOF
+
+filename_ACK_ZIP="$filename_ACK.ZIP"
+zip -r -q "$path_OUT_dir/$filename_ACK_ZIP" "$path_OUT_dir/$filename_ACK"
+#echo "...zipped file ACK: '$filename_ACK_ZIP'"
+#echo
+
+# increment apduIdentifier and fn_PRG_file
+let "C_APDU++"
+fn_PRG_WL="$(expr $fn_PRG_WL + 1)"
+fn_PRG_WL=$(extract_offset_pad $fn_PRG_WL 10)
+
+# to return unix_timestamp !=
+sleep 1
+
+done
 echo
-
-
+# to delete file .XML
+if [[ $FLAG_gen  =~ ^([yY])$ ]] ; then
+    rm "$path_OUT_dir"/*.XML
+fi
 
